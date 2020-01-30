e0bed412474e2fb7638dbb65515e347a57d29afe
Adam Callanan <a.callanan@sbcglobal.net>
2020-01-29 22:47:04 -0600

    cleanup

--- auth/auth.controller.js
+++ auth/auth.controller.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const passport = require('passport');
 const authController = express.Router();
 const authManager = require('./auth.manager');
 const authUtil = require('../utilities/auth.util');
@@ -17,23 +16,3 @@ authController.get('/loggedIn', authUtil.jwtAuthenticated, (req, res) => {
 });
 
 module.exports = authController;
-
-function loginUser(res, req, next) {
-  passport.authenticate('local', (err, user, info) => {
-    let token;
-    if (err) {
-      res.status(404).json(err);
-      return;
-    }
-    if (user) {
-      token = user.generateJwt();
-      res.status(200);
-      res.json({
-        "token": token
-      });
-    }
-    else {
-      res.status(401).json(info);
-    }
-  })(req, res, next);
-}
