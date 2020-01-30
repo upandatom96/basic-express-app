fc9b99a3fe1948d4f0b3e68ad66d254af7e1522d
Adam Callanan <a.callanan@sbcglobal.net>
2020-01-29 22:40:21 -0600

    cleaning up app js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { setupMongo } = require('./mongo-setup');
 const { setupBodyParser } = require('./body-parser-setup');
 const { setupCors } = require('./cors-setup');
 const { setupControllers } = require('./controller-setup');
+const { setupAuth } = require('./auth-setup');
 const { port } = require("./config/env.config");
 
 setupMongo();
@@ -11,21 +12,7 @@ const app = express();
 setupBodyParser(app);
 setupCors(app);
 setupControllers(app);
-
-// setup session
-const session = require('express-session');
-const {secret} = require('./config/env.config');
-app.use(session({
-  secret: secret,
-  resave: true,
-  saveUninitialized: true
-}));
-
-// setup passport
-const passport = require('passport');
-require('./config/passport.config')(passport);
-app.use(passport.initialize());
-app.use(passport.session());
+setupAuth(app);
 
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
---
+++ auth-setup.js
@@ -0,0 +1,26 @@
+const session = require('express-session');
+const { secret } = require('./config/env.config');
+const passport = require('passport');
+
+function setupAuth(app) {
+  setupSession(app);
+  setupPassport(app);
+}
+
+function setupSession(app) {
+  app.use(session({
+    secret: secret,
+    resave: true,
+    saveUninitialized: true
+  }));
+}
+
+function setupPassport(app) {
+  require('./config/passport.config')(passport);
+  app.use(passport.initialize());
+  app.use(passport.session());
+}
+
+module.exports = {
+  setupAuth
+}
