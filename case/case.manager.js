fc5d1fc887c3bbfcc970fcc3e972fb0617baa5a9
Adam Callanan <a.callanan@sbcglobal.net>
2020-02-19 19:22:16 -0600

    uses provided counts

--- case/case.manager.js
+++ case/case.manager.js
@@ -14,11 +14,13 @@ function makeCase(caseOrder) {
     if (errors.length > 0) {
       reject(errors);
     } else {
+      const witnesses = pickWitnesses(caseOrder.witnessCount);
+      const evidence = pickEvidence(caseOrder.evidenceCount);
       const newCase = {
-        name: myCase.name,
-        witnesses: [],
-        plantiffEvidence: [],
-        defendantEvidence: []
+        name: caseOrder.name,
+        witnesses,
+        plantiffEvidence: evidence.plantiffEvidence,
+        defendantEvidence: evidence.defendantEvidence
       };
       resolve(
         newCase
@@ -30,4 +32,33 @@ function makeCase(caseOrder) {
 module.exports = {
   getAllCases,
   makeCase
+}
+
+function pickWitnesses(witnessCount) {
+  const witnesses = [];
+  for (i = 0; i < witnessCount; i++) {
+    const newWitness = {
+      "name": "witness " + (i + 1)
+    };
+    witnesses.push(newWitness);
+  }
+  return witnesses;
+}
+
+function pickEvidence(evidenceCount) {
+  const evidence = {
+    plantiffEvidence: [],
+    defendantEvidence: []
+  };
+  for (i = 0; i < evidenceCount; i++) {
+    const newEvidenceP = {
+      "name": "evidence p " + (i + 1)
+    };
+    evidence.plantiffEvidence.push(newEvidenceP);
+    const newEvidenceD = {
+      "name": "evidence d " + (i + 1)
+    };
+    evidence.defendantEvidence.push(newEvidenceD);
+  }
+  return evidence;
 }
