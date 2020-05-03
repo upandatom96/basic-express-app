91e75606d70148b35c1f11e1d3b3dfed03d52356
Adam Callanan <a.callanan@sbcglobal.net>
2020-05-03 17:30:22 -0500

    finished make tweet

--- tweet/tweet.manager.js
+++ tweet/tweet.manager.js
@@ -2,23 +2,16 @@ const Twit = require('twit');
 const { twitCredentials } = require("../config/twit.config");
 const twitConnection = new Twit(twitCredentials);
 
-let status = "hello, world";
-
 function makeTweet(message) {
-  status = message;
   twitConnection.get('account/verify_credentials', {
     include_entities: false,
     skip_status: true,
     include_email: false
-  }, afterAuth)
+  }, afterAuth(message))
 }
 
-function afterAuth(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    twitConnection.post('statuses/update', { status: status });
-  }
+function afterAuth(message) {
+  twitConnection.post('statuses/update', { status: message });
 }
 
 module.exports = {
