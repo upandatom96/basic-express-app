57562b726affdb60b519c2b875c536d13022ce47
Adam Callanan <a.callanan@sbcglobal.net>
2020-05-09 15:43:43 -0500

    no more issue description

--- issue/Issue.model.js
+++ issue/Issue.model.js
@@ -6,10 +6,6 @@ const IssueSchema = new Schema({
     type: String,
     required: true,
   },
-  description: {
-    type: String,
-    required: false,
-  },
   date: {
     type: Date,
     default: Date.now
