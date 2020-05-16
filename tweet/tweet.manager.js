263cd2dc76a1108f6f5a309c11a1a336ac33738b
Adam Callanan <a.callanan@sbcglobal.net>
2020-05-16 13:46:04 -0500

    rework twit

--- tweet/tweet.manager.js
+++ tweet/tweet.manager.js
@@ -1,11 +1,11 @@
 const Twit = require('twit');
 const { twitCredentials, test } = require("../config/twit.config");
-const twitConnection = new Twit(twitCredentials);
 
 function makeTweet(message) {
   if (test === "true") {
     return;
   }
+  const twitConnection = new Twit(twitCredentials);
   twitConnection.get('account/verify_credentials', {
     include_entities: false,
     skip_status: true,
