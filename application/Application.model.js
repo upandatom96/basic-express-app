79333de8b41be3164a6f6a953c542b69a5a68265
Adam Callanan <a.callanan@sbcglobal.net>
2020-05-28 20:20:25 -0500

    change tagline req

--- application/Application.model.js
+++ application/Application.model.js
@@ -2,43 +2,43 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ApplicationSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  link: {
-    type: String,
-    required: false,
-  },
-  tech: [
-    {
-      type: String
-    }
-  ],
-  githubLink: {
-    type: String,
-    required: false,
-  },
-  tagline: {
-    type: String,
-    required: false,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  icon: {
-    type: String,
-    required: false,
-  },
-  status: {
-    type: Number,
-    default: 0
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  },
+    name: {
+        type: String,
+        required: true,
+    },
+    link: {
+        type: String,
+        required: false,
+    },
+    tech: [
+        {
+            type: String
+        }
+    ],
+    githubLink: {
+        type: String,
+        required: false,
+    },
+    tagline: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    icon: {
+        type: String,
+        required: false,
+    },
+    status: {
+        type: Number,
+        default: 0
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
 });
 
 mongoose.model('application', ApplicationSchema);
