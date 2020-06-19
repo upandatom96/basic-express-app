30a439a201dcd294acbb3a3cc62e94de60d3817e
Adam Callanan <a.callanan@sbcglobal.net>
2020-06-19 00:17:45 -0500

    announcer cleanup

--- clue-bot/announcer.js
+++ clue-bot/announcer.js
@@ -2,26 +2,18 @@ function makeFinalRevealAnnouncement(clueBot) {
     makeAnnouncement(`THE END. ${clueBot.victim} was killed by ${clueBot.culprit} in the ${clueBot.scene} with the ${clueBot.weapon}!`);
 }
 
+function buildClueDescription(clueBot, nextClue) {
+    return `${nextClue} is not involved with the murder`;
+}
+
 function makeClueAnnouncement(clueBot, nextClue) {
+    const clueAnnouncement = buildClueDescription(clueBot, nextClue);
     const part = clueBot.drawnClues.length;
-    makeAnnouncement(`Clue #${part}/15: ${nextClue}`);
+    makeAnnouncement(`Clue #${part}: ${clueAnnouncement}`);
 }
 
 function makeCrimeAnnouncement(clueBot) {
     makeAnnouncement(`${clueBot.title} begins... ${clueBot.victim} has been killed! Who did it? Where? How?`);
-}
-
-function getOptionText(options) {
-    let optionText = "";
-    options.forEach((option, index) => {
-        optionText = optionText + option;
-        if (index === options.length - 2) {
-            optionText = optionText + ", and ";
-        } else if (index < options.length - 2) {
-            optionText = optionText + ", ";
-        }
-    })
-    return optionText;
 }
 
 function makeSuspectOptionAnnouncement(clueBot) {
@@ -39,11 +31,6 @@ function makeSceneOptionAnnouncement(clueBot) {
     makeAnnouncement("The potential crime scenes are: " + optionText);
 }
 
-function makeAnnouncement(announcement) {
-    console.log(announcement);
-    console.log(announcement.length + " characters");
-}
-
 module.exports = {
     makeSceneOptionAnnouncement,
     makeWeaponOptionAnnouncement,
@@ -52,3 +39,22 @@ module.exports = {
     makeClueAnnouncement,
     makeFinalRevealAnnouncement,
 }
+
+function getOptionText(options) {
+    let optionText = "";
+    options.forEach((option, index) => {
+        optionText = optionText + option;
+        if (index === options.length - 2) {
+            optionText = optionText + ", and ";
+        } else if (index < options.length - 2) {
+            optionText = optionText + ", ";
+        }
+    })
+    return optionText;
+}
+
+function makeAnnouncement(announcement) {
+    console.log(announcement);
+    console.log(announcement.length + " characters");
+}
+
