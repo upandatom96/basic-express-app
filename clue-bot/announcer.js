2d96025e4934fc3ac70255caa5411dc3fe224343
Adam Callanan <a.callanan@sbcglobal.net>
2020-06-19 00:54:22 -0500

    better clues

--- clue-bot/announcer.js
+++ clue-bot/announcer.js
@@ -1,9 +1,7 @@
+const randomUtil = require('../utilities/random.util');
+
 function makeFinalRevealAnnouncement(clueBot) {
     makeAnnouncement(`THE END. ${clueBot.victim} was killed by ${clueBot.culprit} in ${clueBot.scene} with ${clueBot.weapon}!`);
-}
-
-function buildClueDescription(clueBot, nextClue) {
-    return `${nextClue} is not involved with the murder`;
 }
 
 function makeClueAnnouncement(clueBot, nextClue) {
@@ -56,5 +54,66 @@ function getOptionText(options) {
 function makeAnnouncement(announcement) {
     console.log(announcement);
     console.log(announcement.length + " characters");
+}
+
+function provideCulpritClue(nextClue, fakeClue) {
+    const randomNumber = randomUtil.pickRandom([1, 2, 3, 4, 5]);
+    if (randomNumber === 1) {
+        return `${nextClue} and ${fakeClue} were talking at the time of the murder`;
+    } else if (randomNumber === 2) {
+        return `${nextClue} and ${fakeClue} were asleep at the time of the murder`;
+    } else if (randomNumber === 3) {
+        return `${nextClue} and ${fakeClue} were performing at the time of the murder`;
+    } else if (randomNumber === 4) {
+        return `${nextClue} and ${fakeClue} were committing unrelated crimes at the time of the murder`;
+    } else {
+        return `${nextClue} and ${fakeClue} were kissing at the time of the murder`;
+    }
+}
+
+function provideSceneClue(nextClue, fakeClue) {
+    const randomNumber = randomUtil.pickRandom([1, 2, 3, 4, 5]);
+    if (randomNumber === 1) {
+        return `${nextClue} and ${fakeClue} were empty at the time of the murder`;
+    } else if (randomNumber === 2) {
+        return `${nextClue} and ${fakeClue} were being cleaned at the time of the murder`;
+    } else if (randomNumber === 3) {
+        return `${nextClue} and ${fakeClue} were under surveillance at the time of the murder`;
+    } else if (randomNumber === 4) {
+        return `${nextClue} and ${fakeClue} were full of party-goers at the time of the murder`;
+    } else {
+        return `${nextClue} and ${fakeClue} were locked at the time of the murder`;
+    }
+}
+
+function provideWeaponClue(nextClue, fakeClue) {
+    const randomNumber = randomUtil.pickRandom([1, 2, 3, 4, 5]);
+    if (randomNumber === 1) {
+        return `${nextClue} and ${fakeClue} were locked away at the time of the murder`;
+    } else if (randomNumber === 2) {
+        return `${nextClue} and ${fakeClue} were being used by party-goers at the time of the murder`;
+    } else if (randomNumber === 3) {
+        return `${nextClue} and ${fakeClue} were on display at the time of the murder`;
+    } else if (randomNumber === 4) {
+        return `${nextClue} and ${fakeClue} were not near the home at the time of the murder`;
+    } else {
+        return `${nextClue} and ${fakeClue} were being used by staff at the time of the murder`;
+    }
+}
+
+function buildClueDescription(clueBot, nextClue) {
+    const isWeapon = clueBot.weaponOptions.includes(nextClue);
+    const isScene = clueBot.sceneOptions.includes(nextClue);
+
+    if (isWeapon) {
+        const fakeClue = clueBot.fakeWeapons.shift();
+        return provideWeaponClue(nextClue, fakeClue);
+    } else if (isScene) {
+        const fakeClue = clueBot.fakeScenes.shift();
+        return provideSceneClue(nextClue, fakeClue);
+    } else {
+        const fakeClue = clueBot.fakeCulprits.shift();
+        return provideCulpritClue(nextClue, fakeClue);
+    }
 }
 
--- utilities/random.util.js
+++ utilities/random.util.js
@@ -1,40 +1,40 @@
 function generateRandomPassword() {
-  var result = '';
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
-  for (var i = 0; i < 10; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < 10; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
 }
 
 function pickRandom(array) {
-  const shuffledArray = shuffleArray(array);
-  return shuffledArray[0];
+    const shuffledArray = shuffleArray(array);
+    return shuffledArray[0];
 }
 
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffleArray(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
 
-  return array;
+    return array;
 }
 
 module.exports = {
-  generateRandomPassword,
-  shuffleArray,
-  pickRandom
+    generateRandomPassword,
+    shuffleArray,
+    pickRandom
 }
