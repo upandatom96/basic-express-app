e1990e330b1fcd3e07e924bdf927f0b4f99dd359
Adam Callanan <a.callanan@sbcglobal.net>
2020-06-18 22:18:53 -0500

    generator cleanup

--- clue-bot/clue-bot-generator.js
+++ clue-bot/clue-bot-generator.js
@@ -1,9 +1,36 @@
 const randomManager = require('../random/random.manager');
+const stringUtil = require('../utilities/string.util');
+const randomUtil = require('../utilities/random.util');
 
-function generateClueBotDetails() {
-    console.log("Generating mystery...");
+function makeTitle() {
+    const adjective = stringUtil.toTitleCase(randomManager.pickAdjective());
+    return `The ${adjective} Mystery`;
+}
 
-    const allCharacters = [
+function selectWeapons() {
+    return [
+        "Weapon One",
+        "Weapon Two",
+        "Weapon Three",
+        "Weapon Four",
+        "Weapon Five",
+        "Weapon Six",
+    ];
+}
+
+function selectScenes() {
+    return [
+        "Scene One",
+        "Scene Two",
+        "Scene Three",
+        "Scene Four",
+        "Scene Five",
+        "Scene Six",
+    ];
+}
+
+function selectCharacters() {
+    return [
         "Character One",
         "Character Two",
         "Character Three",
@@ -12,31 +39,28 @@ function generateClueBotDetails() {
         "Character Six",
         "Character Seven",
     ];
-    const allScenes = [
-        "Scene One",
-        "Scene Two",
-        "Scene Three",
-        "Scene Four",
-        "Scene Five",
-        "Scene Six",
-    ];
-    const allWeapons = [
-        "Weapon One",
-        "Weapon Two",
-        "Weapon Three",
-        "Weapon Four",
-        "Weapon Five",
-        "Weapon Six",
-    ];
-    const title = randomManager.pickAdjective();
+}
+
+function shuffleCluesTogether(allCharacters, allScenes, allWeapons) {
+    const combinedClues = allCharacters.concat(allScenes).concat(allWeapons);
+    return randomUtil.shuffleArray(combinedClues);
+}
+
+function generateClueBotDetails() {
+    const title = makeTitle();
+
+    console.log(`Generating ${title}...`);
+
+    const allCharacters = selectCharacters();
+    const allScenes = selectScenes();
+    const allWeapons = selectWeapons();
 
     const victimName = allCharacters.shift();
     const culpritName = allCharacters.shift();
     const sceneName = allScenes.shift();
-
     const weaponName = allWeapons.shift();
 
-    const unDrawnClues = allCharacters.concat(allScenes).concat(allWeapons);
+    const unDrawnClues = shuffleCluesTogether(allCharacters, allScenes, allWeapons);
 
     return {
         concluded: false,
