6c7bf9ebf5b741f17aa81bf7e1a49b2bdd45a69d
Adam Callanan <a.callanan@sbcglobal.net>
2020-06-18 23:53:21 -0500

    initial announcements

--- clue-bot/clue-bot-progressor.js
+++ clue-bot/clue-bot-progressor.js
@@ -1,25 +1,25 @@
 function progressClue(clueBot) {
     if (clueBot.status === 0) {
         clueBot.status = 1;
-        makeAnnouncement("THE CRIME");
+        makeAnnouncement(`Oh No! ${clueBot.victim} has been killed!`);
     } else if (clueBot.status === 1) {
         clueBot.status = 2;
-        makeAnnouncement("THE OPTIONS: CULPRITS");
+        makeAnnouncement("The suspects are: ");
     } else if (clueBot.status === 2) {
         clueBot.status = 3;
-        makeAnnouncement("THE OPTIONS: WEAPONS");
+        makeAnnouncement("The potential murder weapons are:");
     } else if (clueBot.status === 3) {
         clueBot.status = 4;
-        makeAnnouncement("THE OPTIONS: PLACES");
+        makeAnnouncement("The potential crime scenes are:");
     } else if (clueBot.status === 4) {
         if (clueBot.unDrawnClues.length === 0) {
             clueBot.status = 5;
-            makeAnnouncement("THE REVEAL");
+            makeAnnouncement(`It was ${clueBot.culprit} in the ${clueBot.scene} with the ${clueBot.weapon}!`);
         } else {
             const nextClue = clueBot.unDrawnClues.shift();
             clueBot.drawnClues.push(nextClue);
             const part = clueBot.drawnClues.length;
-            makeAnnouncement(`THE INVESTIGATION (${part}/15)`);
+            makeAnnouncement(`Clue #${part}/15: ${nextClue}`);
         }
     }
     return clueBot;
