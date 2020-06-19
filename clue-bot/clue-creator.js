351c3510da0bec9ae315a4a4836edec762bdbf80
Adam Callanan <a.callanan@sbcglobal.net>
2020-06-19 16:03:19 -0500

    clue base constants

--- clue-bot/clue-creator.js
+++ clue-bot/clue-creator.js
@@ -1,36 +1,6 @@
+import {CULPRIT_CLUE_BASES, SCENE_CLUE_BASES, WEAPON_CLUE_BASES} from "./constants/clue-bases";
+
 const randomUtil = require('../utilities/random.util');
-
-const CULPRIT_CLUE_BASES = [
-    `{CLUE1} and {CLUE2} were talking at the time of the murder`,
-    `{CLUE1} and {CLUE2} were asleep at the time of the murder`,
-    `{CLUE1} was singing a duet with {CLUE2} at the time of the murder`,
-    `{CLUE1} and {CLUE2} were committing unrelated crimes at the time of the murder`,
-    `{CLUE1} was kissing {CLUE2} at the time of the murder`,
-    `{CLUE} was locked in a closet all night`,
-    `{CLUE} never showed up to the party`,
-    `{CLUE} passed out before the murder occurred`,
-];
-
-const SCENE_CLUE_BASES = [
-    `{CLUE1} and {CLUE2} were empty at the time of the murder`,
-    `{CLUE1} and {CLUE2} were being cleaned at the time of the murder`,
-    `{CLUE1} and {CLUE2} were under surveillance at the time of the murder`,
-    `{CLUE1} and {CLUE2} were full of party-goers at the time of the murder`,
-    `{CLUE1} and {CLUE2} were locked at the time of the murder`,
-    `{CLUE} was closed for remodelling all night`,
-    `{CLUE} is a holy space not fit for murder`,
-    `{CLUE} would have been too obvious a place for a murder`,
-];
-
-const WEAPON_CLUE_BASES = [
-    `{CLUE1} and {CLUE2} were locked away at the time of the murder`,
-    `{CLUE1} and {CLUE2} were being used by party-goers at the time of the murder`,
-    `{CLUE1} and {CLUE2} were on display at the time of the murder`,
-    `{CLUE1} and {CLUE2} were not near the home at the time of the murder`,
-    `{CLUE1} and {CLUE2} were being used by staff at the time of the murder`,
-    `{CLUE} was in plain sight all night long`,
-    `{CLUE} was being guarded by a dog all night`,
-];
 
 function createClue(clueBot, nextClue) {
     const isWeapon = clueBot.weaponOptions.includes(nextClue);
