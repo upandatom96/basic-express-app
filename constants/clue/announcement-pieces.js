81a79df4ac764a5a27ed33fcb24dc4998db280c5
Adam Callanan <a.callanan@sbcglobal.net>
2020-06-24 22:40:48 -0500

    finish announcement piece basics

--- constants/clue/announcement-pieces.js
+++ constants/clue/announcement-pieces.js
@@ -1,20 +1,39 @@
 const INTRO_START_PIECES = [
     "Welcome, detectives!",
+    "Greetings, sleuths!",
+    "Hello, investigators!",
+    "Good day, gumshoes!",
+    "Welcome, private eyes!",
+    "Greetings, inspectors!",
+    "Howdy, agents!",
 ];
 const INTRO_END_PIECES = [
     "A new mystery is about to unfold.",
+    "You have an uneasy feeling that something terrible is about to happen.",
+    "You can feel in the air that a sinister plan is afoot.",
+    "Everything seems quiet in this town, too quiet.",
 ];
 const CRIME_PIECES = [
-    "was killed during a party with many Esteemed Guests"
+    "was killed during a party with many esteemed guests",
+    "was murdered during a party celebrated a new play",
+    "was found dead after a house warming party",
+    "was found dead after a cocktail party",
+    "was killed during a fundraising party",
 ];
 const SUSPECT_OPTION_PIECES = [
     "As you head to the house, you compile a list of people you find suspicious:",
+    "As you approach the home, you are handed a list of potential suspects:",
+    "As reach the front door, a colleague provides you a list of possible culprits:",
 ];
 const WEAPON_OPTION_PIECES = [
     "You find some strange items throughout the house that could have been used to kill:",
+    "Your colleague gives you a list of possible murder weapons:",
+    "You discover a few odd items and write down some which seem potentially deadly:",
 ];
 const SCENE_OPTION_PIECES = [
     "You investigate where the body was found. You take note of nearby rooms:",
+    "You find traces of blood in the following rooms:",
+    "You enter all the rooms in the house and consider some as potential crime scenes:",
 ];
 const INVESTIGATION_PIECES = [
     "you begin searching the house for Clues to eliminate some possibilities",
@@ -29,6 +48,7 @@ const PENULTIMATE_PIECES = [
 const REVEAL_PIECES = [
     "We caught the Culprit!",
     "The Culprit confessed!",
+    "The Culprit was caught red-handed trying to run away!",
 ];
 
 module.exports = {
