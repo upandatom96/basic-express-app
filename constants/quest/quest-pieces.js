52d52ce2834253ab2a42e3cc096cd70f6bc8f469
Adam Callanan <a.callanan@sbcglobal.net>
2020-09-10 15:47:44 -0500

    more quest words

--- constants/quest/quest-pieces.js
+++ constants/quest/quest-pieces.js
@@ -1,5 +1,25 @@
 const QUEST_WORDS = [
     "Quest",
+    "Search",
+    "Hunt",
+    "Pursuit",
+    "Operation",
+    "Assignment",
+    "Task",
+    "Job",
+    "Expedition",
+    "Adventure",
+    "Journey",
+    "Errand",
+    "Chore",
+    "Task",
+    "Calling",
+    "Escapade",
+    "Deed",
+    "Feat",
+    "Trial",
+    "Legend",
+    "Incident",
     "Mission"
 ];
 
