b2a159462f9ecdfa1393a4a4d9ad79ae0f377a50
Adam Callanan <a.callanan@sbcglobal.net>
2020-09-10 21:14:38 -0500

    dummy endpoints

--- quest/quest.controller.js
+++ quest/quest.controller.js
@@ -3,6 +3,43 @@ const questController = express.Router();
 const questManager = require("./quest.manager");
 const authUtil = require('../utilities/auth.util');
 
+questController.get('/currentEpic', async (req, res) => {
+    try {
+        res.send("No current Epic");
+    } catch (err) {
+        res.statusCode = 500;
+        res.send(err);
+    }
+});
+
+questController.get('/epics', async (req, res) => {
+    try {
+        res.send([]);
+    } catch (err) {
+        res.statusCode = 500;
+        res.send(err);
+    }
+});
+
+questController.post('/advanceCurrentEpic', async (req, res) => {
+    try {
+        res.send("Advancing epic...");
+    } catch (err) {
+        res.statusCode = 500;
+        res.send(err);
+    }
+});
+
+questController.delete('/epic/:id', authUtil.jwtAuthenticated, authUtil.jwtAdmin, async (req, res) => {
+    try {
+        const id = req.params.id;
+        res.send("Deleting epic " + id + "...");
+    } catch (err) {
+        res.statusCode = 500;
+        res.send(err);
+    }
+});
+
 questController.get('/randomQuestName', async (req, res) => {
     try {
         const name = await questManager.getRandomQuestName();
