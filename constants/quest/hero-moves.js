c81eaab907d6d5ff76df3b66eacacb30b54a27c7
Adam Callanan <a.callanan@sbcglobal.net>
2020-10-05 15:00:05 -0500

    add moves

--- constants/quest/hero-moves.js
+++ constants/quest/hero-moves.js
@@ -3,41 +3,65 @@ const MoveTypes = require('./move-types');
 const STANDARD_MOVES = [
     {
         type: MoveTypes.HEAL,
-        name: "HERO HEAL",
+        name: "HEROIC HEAL",
         multiplier: 1,
     },
     {
         type: MoveTypes.STRENGTH_ATTACK,
-        name: "HERO PUNCH",
+        name: "HEROIC PUNCH",
         multiplier: 1,
     },
     {
         type: MoveTypes.STRENGTH_ATTACK,
-        name: "HERO KICK",
+        name: "HEROIC KICK",
         multiplier: 2,
     },
     {
         type: MoveTypes.DEXTERITY_ATTACK,
-        name: "HERO SLAP",
+        name: "HEROIC SLAP",
         multiplier: 1,
     },
     {
         type: MoveTypes.FAIL,
-        name: "HERO TRIP",
+        name: "HEROIC FLASHBACK",
     },
 ];
 
 const SPECIAL_MOVES = [
     {
-        code: "CLEVER_TRAP",
+        code: "DIFFICULT_RIDDLE",
         type: MoveTypes.WISDOM_ATTACK,
-        name: "CLEVER TRAP",
+        name: "DIFFICULT RIDDLE",
+        multiplier: 1,
+    },
+    {
+        code: "BACKHANDED_COMPLIMENT",
+        type: MoveTypes.CHARISMA_ATTACK,
+        name: "BACKHANDED_COMPLIMENT",
+        multiplier: 1,
+    },
+    {
+        code: "HEAD_BANG",
+        type: MoveTypes.STRENGTH_ATTACK,
+        name: "HEAD BANG",
+        multiplier: 1,
+    },
+    {
+        code: "NOSE_GRAB",
+        type: MoveTypes.DEXTERITY_ATTACK,
+        name: "NOSE GRAB",
+        multiplier: 1,
+    },
+    {
+        code: "MAGIC_DART",
+        type: MoveTypes.WISDOM_ATTACK,
+        name: "MAGIC DART",
         multiplier: 2,
     },
     {
-        code: "POISON_KISS",
+        code: "SCORNFUL_GLARE",
         type: MoveTypes.CHARISMA_ATTACK,
-        name: "POISON KISS",
+        name: "SCORNFUL GLARE",
         multiplier: 2,
     },
     {
@@ -51,6 +75,54 @@ const SPECIAL_MOVES = [
         type: MoveTypes.DEXTERITY_ATTACK,
         name: "ROUNDHOUSE KICK",
         multiplier: 2,
+    },
+    {
+        code: "DELIBERATE_PUNCH",
+        type: MoveTypes.WISDOM_ATTACK,
+        name: "DELIBERATE PUNCH",
+        multiplier: 2,
+    },
+    {
+        code: "WITTY_INSULT",
+        type: MoveTypes.CHARISMA_ATTACK,
+        name: "WITTY INSULT",
+        multiplier: 2,
+    },
+    {
+        code: "SUPLEX",
+        type: MoveTypes.STRENGTH_ATTACK,
+        name: "SUPLEX",
+        multiplier: 2,
+    },
+    {
+        code: "SURPRISE_ATTACK",
+        type: MoveTypes.DEXTERITY_ATTACK,
+        name: "SURPRISE ATTACK",
+        multiplier: 2,
+    },
+    {
+        code: "CLEVER_TRAP",
+        type: MoveTypes.WISDOM_ATTACK,
+        name: "CLEVER TRAP",
+        multiplier: 3,
+    },
+    {
+        code: "POISON_KISS",
+        type: MoveTypes.CHARISMA_ATTACK,
+        name: "POISON KISS",
+        multiplier: 3,
+    },
+    {
+        code: "PILEDRIVER",
+        type: MoveTypes.STRENGTH_ATTACK,
+        name: "PILEDRIVER",
+        multiplier: 3,
+    },
+    {
+        code: "RUNNING_PUNCH",
+        type: MoveTypes.DEXTERITY_ATTACK,
+        name: "RUNNING PUNCH",
+        multiplier: 3,
     },
 ];
 
