55f2eb24f4ea0a9b7f26730410e184d3bb5c4477
Adam Callanan <a.callanan@sbcglobal.net>
2021-01-17 00:05:02 -0600

    read csv to api response

--- vm-archive/vm-archive.manager.js
+++ vm-archive/vm-archive.manager.js
@@ -1,9 +1,44 @@
+const fs = require('fs');
+
+const boolUtil = require('../utilities/bool.util');
+const csvUtil = require('../utilities/csv.util');
+
 function getShows() {
-    return new Promise((resolve, reject) => {
-        resolve([]);
+    return new Promise(async (resolve, reject) => {
+        try {
+            fs.readFile("vm-archive/vm-shows.csv", function read(err, data) {
+                if (err) {
+                    throw err;
+                }
+                const shows = csvUtil.readCsv(data + "");
+                const convertedShows = convertShows(shows);
+                resolve(convertedShows);
+            });
+        } catch (err) {
+            reject(err);
+        }
+
     });
 }
 
 module.exports = {
-    getShows
+    getShows,
+}
+
+function convertShows(shows) {
+    return shows
+        .filter((show) => {
+            return show.year !== "";
+        })
+        .map((show) => {
+            show.soldOut = boolUtil.translateBooleanString(show.soldOut);
+            show.canceled = boolUtil.translateBooleanString(show.canceled);
+            show.postponed = boolUtil.translateBooleanString(show.postponed);
+            show.acts = show.lineup.split("|")
+                .map((act) => {
+                    return act.trim();
+                });
+            show.fullDate = `${show.month} ${show.date}, ${show.year} at ${show.time}`;
+            return show;
+        });
 }
