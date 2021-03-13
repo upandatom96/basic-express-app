3b3f1cc36e0d7d50907d7482d497d10726161687
Adam Callanan <a.callanan@sbcglobal.net>
2021-03-13 01:50:33 -0600

    muppet CRUD

--- muppet/muppet.validator.js
+++ muppet/muppet.validator.js
@@ -3,7 +3,7 @@ const boolUtil = require('../utilities/bool.util');
 function checkForCreateErrors(muppet) {
     const errors = checkForGeneralErrors(muppet);
     if (boolUtil.hasValue(muppet._id)) {
-        errors.push({ text: 'Create cannot have an id.' });
+        errors.push({text: 'Create cannot have an id.'});
     }
     return errors;
 }
@@ -11,19 +11,13 @@ function checkForCreateErrors(muppet) {
 function checkForEditErrors(muppet) {
     const errors = checkForGeneralErrors(muppet);
     if (boolUtil.hasNoValue(muppet._id)) {
-        errors.push({ text: 'Edit must have an id.' });
+        errors.push({text: 'Edit must have an id.'});
     }
     return errors;
 }
 
 function checkForGeneralErrors(muppet) {
-    const fields = [
-        "name",
-        "debutYear",
-        "commonality",
-        "speechType",
-        "mainSeries",
-        "creature",
+    const muppetStats = [
         "starQuality",
         "cuteness",
         "imagination",
@@ -34,6 +28,14 @@ function checkForGeneralErrors(muppet) {
         "floppiness",
         "fuzziness",
         "softness",
+    ];
+    const fields = [
+        "name",
+        "debutYear",
+        "commonality",
+        "speechType",
+        "mainSeries",
+        "creature",
         "strength",
         "wisdom",
         "intelligence",
@@ -44,7 +46,30 @@ function checkForGeneralErrors(muppet) {
         "alignment",
         "archived",
     ];
-    return boolUtil.collectErrors(muppet, fields);
+    const errs = boolUtil.collectErrors(muppet, fields.concat(muppetStats));
+    const COMMONALITIES = ["RARE", "UNCOMMON", "COMMON"];
+    if (muppet.commonality && !COMMONALITIES.includes(muppet.commonality.toUpperCase())) {
+        errs.push("Invalid commonality");
+    }
+    const SPEECH_TYPES = ["NONE", "GIBBERISH", "ENGLISH"];
+    if (muppet.speechType && !SPEECH_TYPES.includes(muppet.speechType.toUpperCase())) {
+        errs.push("Invalid speech type");
+    }
+    const ALIGNMENTS = [
+        "LAWFUL GOOD", "NEUTRAL GOOD", "CHAOTIC GOOD",
+        "LAWFUL NEUTRAL", "TRUE NEUTRAL", "CHAOTIC NEUTRAL",
+        "LAWFUL EVIL", "NEUTRAL EVIL", "CHAOTIC EVIL",
+        "UNALIGNED",
+    ];
+    if (muppet.alignment && !ALIGNMENTS.includes(muppet.alignment.toUpperCase())) {
+        errs.push("Invalid alignment");
+    }
+    muppetStats.forEach((stat) => {
+        if (muppet[stat] && (muppet[stat] < 0 || muppet[stat] > 10)) {
+            errs.push("Invalid " + stat);
+        }
+    });
+    return errs;
 }
 
 module.exports = {
